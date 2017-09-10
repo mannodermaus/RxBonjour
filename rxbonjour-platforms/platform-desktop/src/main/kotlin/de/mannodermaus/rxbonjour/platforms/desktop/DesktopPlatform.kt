496925ea8a6810b591f326cc5b0aa9189283e463
Marcel Schnelle <aurae@web.de>
2017-09-10 10:18:27 +0900

    Started working on Java sample, however classpath is super broken from IDE

--- rxbonjour-platforms/platform-desktop/src/main/kotlin/de/mannodermaus/rxbonjour/platforms/desktop/DesktopPlatform.kt
+++ rxbonjour-platforms/platform-desktop/src/main/kotlin/de/mannodermaus/rxbonjour/platforms/desktop/DesktopPlatform.kt
@@ -1,10 +1,15 @@
 package de.mannodermaus.rxbonjour.platforms.desktop
 
 import de.mannodermaus.rxbonjour.Platform
+import de.mannodermaus.rxbonjour.PlatformConnection
 import io.reactivex.disposables.Disposable
 import java.net.InetAddress
 
 class DesktopPlatform private constructor() : Platform {
+    override fun createConnection(): PlatformConnection {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun getWifiAddress(): InetAddress {
         // TODO How to go about this?
         return InetAddress.getLocalHost()
---
+++ samples/sample-java/src/main/java/de/mannodermaus/rxbonjour/samples/java/Main.java
@@ -0,0 +1,19 @@
+package de.mannodermaus.rxbonjour.samples.java;
+
+import de.mannodermaus.rxbonjour.RxBonjour;
+import de.mannodermaus.rxbonjour.platforms.desktop.DesktopPlatform;
+import de.mannodermaus.rxbonjour.drivers.jmdns.JmDNSDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RxBonjour rxBonjour = new RxBonjour.Builder()
+                .platform(DesktopPlatform.create())
+                .driver(JmDNSDriver.create())
+                .create();
+
+        rxBonjour.newDiscovery("_ssh._tcp")
+                .blockingSubscribe(event -> System.out.println("Event: " + event),
+                        error -> System.err.println("Error: " + error.getMessage()));
+    }
+}
